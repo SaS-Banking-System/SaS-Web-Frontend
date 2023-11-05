90aaeb826303318afa407ff5aeadadc614edc955
johannes-baehr <johannes.baehr@gmx.net>
2023-11-05 18:11:37 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+</style>
---
+++ components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav>
+   <div class="left">
+     <a href="">Logo</a>
+     <a href="">FAQ</a>
+     <a href="">Impressum</a>
+   </div>
+   <div class="right">
+     <a class="login" href="">Mein Konto</a>
+   </div>
+  </nav>
+</template>
+
+<style scoped>
+  nav {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    height: 12vh;
+  }
+  .left, .right {
+    display: flex;
+    align-items: center;
+    margin-inline: 10rem;
+  }
+  .left > a {
+    margin-inline: 2rem;
+  }
+  a {
+    all: unset; 
+    cursor: pointer;
+    font-size: 1.3rem;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  }
+  .login {
+    outline: 1px solid black;
+    padding: 10px;
+    border-radius: 10px;
+  }
+</style>
