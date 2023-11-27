33bc2d421a604cc26876a22de008f7af82526947
best2327 <bkaya1007@gmail.com>
2023-11-27 15:12:03 +0100

    tweaked navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -16,7 +16,7 @@
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    height: 6vh;
+    height: 8vh;
   }
   .left, .right {
     display: flex;
@@ -27,28 +27,35 @@
   a {
     all: unset;
     cursor: pointer;
-    font-size: 1.4rem;
+    font-size: 1.2rem;
     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
   }
 
   .left a {
     margin-inline: 1.5rem;
-    border-radius: 6px;
-    padding: 10px;
+    background-image: linear-gradient(rgb(204, 50, 191),rgb(204, 50, 191));
+    background-size: 0% 2px;
+    background-position: 0% 100%;
+    background-repeat: no-repeat;
+    transition: background-size 0.17s, color 0.17s;
+    color: rgb(0,0,0);
+    padding: 3px;
   }
   .left a:hover {
-    color: rgb(12, 68, 105);
-    background-color: rgb(185, 221, 245);
+    color: rgb(179, 22, 158);
+    background-size: 100% 2px;
   }
+
   .login {
-    background: rgb(43, 43, 43);
-    color: white;
-    font-size: 1.2rem;
-    padding: 5px;
+    border: solid 2px rgba(13, 124, 250, 0.616);
+    transition: background 0.1s;
+    color: rgba(13, 124, 250);
+    font-size: 1rem;
+    padding: 10px;
     border-radius: 5px;
   }
 
   .login:hover {
-    background: rgb(97, 97, 97);
+    background: rgba(13, 155, 250, 0.123);
   }
 </style>
